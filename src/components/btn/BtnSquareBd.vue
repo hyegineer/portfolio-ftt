21151258c46e0730fc07e2767e36aa6c4e5d6140
jeonghyejin <pocket.jhj@gmail.com>
2021-10-06 15:22:31 +0900

    jhj 페이지네이션 구성요소 제작 완료

---
+++ src/components/btn/BtnSquareBd.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    type="button"
+    class="btn-square-bd"
+    @click="click"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  methods: {
+    click() {
+      this.$emit('click');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
