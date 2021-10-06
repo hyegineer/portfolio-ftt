07f1a700a1718e97984495268b1abdc62b4f0929
jeonghyejin <pocket.jhj@gmail.com>
2021-10-06 15:51:14 +0900

    jhj 회원가입 관련 페이지 세팅

---
+++ src/views/auth/RegisterFactChecker.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page">
+    <!-- [D] 팩트체커 회원가입 -->
+    <div>
+      팩트체커 계정정보 등록
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/auth/RegisterIntroInvestor.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page">
+    <!-- [D] 투자자 회원가입 intro: 개인인지 기업인지 -->
+    <div />
+  </div>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
