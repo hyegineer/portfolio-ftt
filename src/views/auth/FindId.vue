cb88840cde564ef17cd6c48a882be11507ee9e85
pocket.cgs <pocket.cgs@gmail.com>
2021-10-06 16:14:54 +0900

    cgs - find-id 작업중

--- src/views/auth/FindId.vue
+++ src/views/auth/FindId.vue
@@ -20,6 +20,47 @@
           >
         </div>
       </div>
+      <div>
+        <label class="lbel">휴대폰 번호</label>
+        <div class="inp-grp inp-btn-grp">
+          <input
+            type="text"
+            class="inp"
+            placeholder="검색어를 입력하세요"
+          >
+
+          <btn-bd class="btn-md">
+            인증번호 발송
+          </btn-bd>
+        </div>
+
+        <div class="inp-grp inp-btn-grp">
+          <div class="inp-grp inp-time-grp">
+            <input
+              type="text"
+              class="inp"
+              placeholder="검색어를 입력하세요"
+            >
+            <span class="inp-time">3:00</span>
+          </div>
+
+          <button
+            type="button"
+            class="btn-solid-dark btn-md"
+          >
+            인증하기
+          </button>
+        </div>
+        <p class="confirm-ment">
+          인증이 완료되었습니다.
+        </p>
+      </div>
+      <btn-solid
+        class="btn-lg btn-full"
+        @click="window.alert('다음 버튼을 클릭하셨습니다.')"
+      >
+        다음
+      </btn-solid>
     </div>
     <layout-footer />
   </div>
