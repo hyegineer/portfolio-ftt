70d60f9a640edf5170ee2d9d8f3715850bf18e86
jeonghyejin <pocket.jhj@gmail.com>
2021-10-06 15:53:37 +0900

    jhj 회원가입 인트로 시작

--- src/views/auth/RegisterIntro.vue
+++ src/views/auth/RegisterIntro.vue
@@ -1,10 +1,19 @@
 <template>
-  <div />
+  <div class="page">
+    <div>
+      <h3>회원가입</h3>
+      <p>회원가입을 환영합니다! <br>원하시는 회원 유형을 선택하세요.</p>
+
+      <btn-solid class="btn-lg">
+        다음
+      </btn-solid>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    
+  name: 'RegisterIntro',
 };
 </script>
 
