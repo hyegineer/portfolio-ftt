a0fc8ff8d850336659e7a602dba1d2d4c0a00434
pocket.cgs <pocket.cgs@gmail.com>
2021-10-06 12:42:00 +0900

    cgs - 로그인 작업중

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="page">
+    <layout-header />
+    <div class="narrow-panel auth-panel">
+      <div
+        class="banner-wrap"
+        style="border: 5px solid orange"
+      >
+        배너영역 420X160
+      </div>
+      <p class="big-ttl">
+        <b class="bold-txt">팩트시트에 로그인</b>하고,<br>다양한 서비스를 이용해보세요!
+      </p>
+      <div class="input-grp">
+        <input
+          type="text"
+          class="input"
+        >
+        <label
+          for=""
+          class="label"
+        >이메일 아이디</label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+</style>
