be1c5e9dcf2719ec863b2cacd68e2c1af6b51906
pocket.cgs <pocket.cgs@gmail.com>
2021-10-08 16:35:09 +0900

    cgs

--- src/views/user-startup/my-info/SMyInfoMain.vue
+++ src/views/user-startup/my-info/SMyInfoMain.vue
@@ -1,7 +1,20 @@
 <template>
   <!-- [D] 마이페이지 - 내정보 -->
-  <div>
-    내정보
+  <div class="page myinfo-page">
+    <div class="wide-panel">
+      <div class="name-box">
+        <p class="name">
+          김철수님의<br>계정 정보입니다.
+        </p>
+        <btn-bd-dark
+          class="btn-sm btn-edit"
+          @click="window.alert('수정하기 버튼을 클릭하셨습니다.')"
+        >
+          수정하기
+        </btn-bd-dark>
+      </div>
+      <div class="info-box" />
+    </div>
   </div>
 </template>
 
