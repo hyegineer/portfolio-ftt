e57bed50ca70fc31e71df20b1598ab9b861a485c
jeonghyejin <pocket.jhj@gmail.com>
2021-10-08 18:03:36 +0900

    jhj 팩트체크, 쪽지함 제목 / 내용 영역 세팅

--- src/views/user-startup/SMypage.vue
+++ src/views/user-startup/SMypage.vue
@@ -153,6 +153,7 @@
               <router-link
                 class="lnb-btn"
                 to="/startup/mypage/ftck-apply-main"
+                :class="{'router-link-active': $route.path.startsWith(`/startup/mypage/ftck-apply`)}"
               >
                 팩트체커 신청
               </router-link>
@@ -160,6 +161,7 @@
               <router-link
                 class="lnb-btn"
                 to="/startup/mypage/ftck-list"
+                :class="{'router-link-active': $route.path.startsWith(`/startup/mypage/ftck-detail`)}"
               >
                 우리 회사가 받은 팩트체크
               </router-link>
@@ -171,6 +173,7 @@
             <router-link
               to="/startup/mypage/note-list"
               class="mypage-snav-btn"
+              :class="{'router-link-active': $route.path.startsWith(`/startup/mypage/note`)}"
             >
               <span class="txt">쪽지함</span>
               <i class="arrow" />
--- src/views/user-startup/note/SNoteList.vue
+++ src/views/user-startup/note/SNoteList.vue
@@ -1,7 +1,31 @@
 <template>
   <!-- [D] 마이페이지 - 쪽지함 목록 -->
-  <div>
-    마이페이지 - 쪽지함 목록
+  <div class="mypage-container">
+    <!-- 마이페이지 페이지 제목 -->
+    <div class="mypage-hgrp">
+      <breadcrumb>
+        <li class="txt">
+          홈
+        </li>
+        <li class="txt">
+          마이페이지
+        </li>
+        <li class="txt">
+          쪽지함
+        </li>
+      </breadcrumb>
+
+      <h2 class="ttl">
+        쪽지함
+      </h2>
+    </div>
+    <!-- // 마이페이지 페이지 제목 -->
+
+    <!-- 마이페이지 컨텐츠 영역 -->
+    <div class="mypage-cont-wrap">
+      받은쪽지 , 보낸 쪽지
+    </div>
+    <!-- // 마이페이지 컨텐츠 영역 -->
   </div>
 </template>
 
