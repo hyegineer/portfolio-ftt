6253fc0949d7c09473ec9c6fb042cab9c2abe113
jeonghyejin <pocket.jhj@gmail.com>
2021-10-12 17:45:48 +0900

    jhj 우리회사가받은팩트체크 리스트 완료

--- src/views/user-startup/factcheck/SFtckList.vue
+++ src/views/user-startup/factcheck/SFtckList.vue
@@ -63,13 +63,131 @@
       </div>
       <!-- // 팩트체크 인트로박스 -->
 
+      <!-- 팩트체크가 있을 때 -->
+      <table class="ftck-list-grp board-list-grp">
+        <!-- [D] 비공개면 is-secret 클래스 추가 -->
+        <tr class="board-list is-secret">
+          <td class="td-subject">
+            <div class="ftck-state-subject">
+              <div class="ftck-state">
+                <badge-state txt="진행중" />
+
+                <!-- [D] 비공개일 때만 보임 -->
+                <div class="badge-secret">
+                  <i class="icon icon-secret" />
+                  <span class="txt">비공개</span>
+                </div>
+              </div>
+
+              <div class="ftck-subject">
+                <span class="lbel">{{ '항목명' }}</span>
+                <p class="subject">
+                  {{ '비공개 팩트체크입니다.' }}
+                </p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <span class="td-word-writer">{{ '작성자명' }}</span>
+          </td>
+          <td>
+            <span class="td-word-date">{{ '2021.00.00' }}</span>
+          </td>
+        </tr>
+        
+        <!-- [D] 완료 -->
+        <tr class="board-list">
+          <td class="td-subject">
+            <div class="ftck-state-subject">
+              <div class="ftck-state">
+                <badge-state txt="완료" />
+
+                <!-- [D] 비공개일 때만 보임 -->
+                <!-- <div class="badge-secret">
+                  <i class="icon icon-secret" />
+                  <span class="txt">비공개</span>
+                </div> -->
+              </div>
+
+              <div class="ftck-subject">
+                <span class="lbel">{{ '항목명' }}</span>
+                <p class="subject">
+                  {{ '팩트체크 요청사항 제목입니다. 두 줄일리 없겠지만 두 줄일 때는 말줄임표시가 나옵니다. 팩트체크 요청사항 제목입니다. 두 줄일리 없겠지만 두 줄일 때는 말줄임표시가 나옵니다.' }}
+                </p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <span class="td-word-writer">{{ '작성자명' }}</span>
+          </td>
+          <td>
+            <span class="td-word-date">{{ '2021.00.00' }}</span>
+          </td>
+        </tr>
+      </table>
+      <!-- // 받은 팩트체크가 있을 때 -->
+
+      <!-- 팩트체크가 없을 때 -->
       <div class="ftck-list-grp">
-        <div class="ftck-list">
-          <div class="subject-grp">
-            <badge-state txt="진행중" />
-          </div>
-        </div>
+        <empty-contents txt="받은 팩트체크가 없습니다." />
       </div>
+      <!-- // 받은 팩트체크가 없을 때 -->
+
+      <!-- 페이지네이션 -->
+      <div class="pagination">
+        <btn-square-bd @click="window.alert('이전 버튼 누르기')">
+          <i class="icon icon-page-prev" />
+          <span class="is-voice-only">이전</span>
+        </btn-square-bd>
+
+        <button
+          type="button"
+          class="num-btn is-active"
+        >
+          1
+        </button>
+        
+        <button
+          type="button" 
+          class="num-btn"
+        >
+          2
+        </button>
+        
+        <button
+          type="button" 
+          class="num-btn"
+        >
+          3
+        </button>
+        
+        <button
+          type="button" 
+          class="num-btn"
+        >
+          4
+        </button>
+
+        <button
+          type="button" 
+          class="num-btn"
+        >
+          ···
+        </button>
+        
+        <button
+          type="button" 
+          class="num-btn"
+        >
+          10
+        </button>
+        
+        <btn-square-bd @click="window.alert('다음 버튼 누르기')">
+          <i class="icon icon-page-next" />
+          <span class="is-voice-only">다음</span>
+        </btn-square-bd>
+      </div>
+      <!-- // 페이지네이션 -->
     </div>
     <!-- // 마이페이지 컨텐츠 영역 -->
   </div>
