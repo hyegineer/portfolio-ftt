a4a054d15ce3def4d4d82603fe8a71645bf692df
key <key9559@naver.com>
2021-10-12 18:02:09 +0900

    key 상세정보까지

--- src/views/user-common/EtprProfileBoard.vue
+++ src/views/user-common/EtprProfileBoard.vue
@@ -167,6 +167,236 @@
           </div>
         </div>
       </div>
+      <!-- 정보 영역 -->
+      <div class="wide-panel">
+        <!-- 기업정보 -->
+        <div class="etpr-main-conts">
+          <h2 class="section-ttl">기업정보</h2>
+          <div class="flexable-container">
+            <div class="etpr-panel-wrap flexable-inner">
+              <div class="panel-ttl">
+                <span>기업 기본정보</span>
+                컴포넌트에서 가져오기
+                    <!-- TODO: 업데이트 데이터. 팩트체커 green, 자사 yellow, 오래된 항목은 badge만 -->
+                <p class="badge green">{{'팩트체커 업데이트  2020.00.00'}}</p>
+              </div>
+              <div class="white-panel">
+                <div class="common-info-container">
+                  <ul class="common-info-list-wrap">
+                    <!-- TODO: 정보가 없는 곳은 전부 - 로 표시 -->
+                    <li class="common-info-list">
+                      <h4 class="ttl">대표자</h4>
+                      <div class="dec">{{'홍길동'}}</div>
+                    </li>
+                    <li class="common-info-list">
+                      <h4 class="ttl">사업자번호</h4>
+                      <div class="dec">{{'000-00-00***'}}</div>
+                    </li>
+                    <li class="common-info-list">
+                      <h4 class="ttl">본사주소</h4>
+                      <div class="dec">{{'서울특별시 강남구 테헤란로 312 13층 주소길어지면 최대 2줄 (우편번호)'}}</div>
+                    </li>
+                    <li class="common-info-list">
+                      <h4 class="ttl">전화번호</h4>
+                      <div class="dec">{{'02-1234-5678'}}</div>
+                    </li>
+                    <li class="common-info-list">
+                      <h4 class="ttl">기업규모</h4>
+                      <div class="dec">{{'소기업'}}</div>
+                    </li>
+                    <li class="common-info-list">
+                      <h4 class="ttl">산업분류</h4>
+                      <div class="dec">{{'ICT 서비스'}}</div>
+                    </li>
+                    <li class="common-info-list">
+                      <h4 class="ttl">산업분야</h4>
+                      <div class="dec">{{'-'}}</div>
+                    </li>
+                    <li class="common-info-list">
+                      <h4 class="ttl">설립일</h4>
+                      <div class="dec">{{'2021년 03월 10일'}}</div>
+                    </li>
+                    <li class="common-info-list tag-list">
+                      <h4 class="ttl">태그</h4>
+                      <div class="dec">
+                        <div class="tag-badge">태그최대여덟글자</div>
+                        <div class="tag-badge">태그최대</div>
+                        <div class="tag-badge">여덟글자</div>
+                        <div class="tag-badge">두줄까지표시가능</div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="etpr-panel-wrap flexable-inner">
+              <div class="panel-ttl">
+                <span>기업가치</span>
+                컴포넌트에서 가져오기
+                    <!-- TODO: 업데이트 데이터. 팩트체커 green, 자사 yellow, 오래된 항목은 badge만 -->
+                <p class="badge yellow">{{'자사 업데이트  2020.00.00'}}</p>
+              </div>
+              <div class="white-panel">
+                <div class="common-info-container">
+                  <p class="common-info-graph">그래프 들어옵니다.</p>
+                </div>
+              </div>
+              <div class="bot-txt">투자자가 작성한 리뷰 기반의 평균 지수입니다.</div>
+            </div>
+          </div>
+        </div>
+        <!-- END 기업정보 -->
+        <!-- 상세정보 -->
+        <div class="etpr-main-conts">
+          <h2 class="section-ttl">상세정보</h2>
+          <div class="flexable-container">
+            <div class="etpr-panel-wrap flexable-inner">
+              <div class="panel-ttl">
+                <span>규모</span>
+                컴포넌트에서 가져오기
+                    <!-- TODO: 업데이트 데이터. 팩트체커 green, 자사 yellow, 오래된 항목은 badge만 -->
+                <p class="badge">{{'팩트체커 업데이트  2020.00.00'}}</p>
+              </div>
+              <div class="white-panel">
+                <div class="common-info-container">
+                  <ul class="common-info-list-wrap">
+                    <!-- TODO: 정보가 없는 곳은 전부 - 로 표시 -->
+                    <li class="common-info-list scale-list">
+                      <h4 class="ttl">발행주식 수</h4>
+                      <div class="dec">{{'-'}} 주</div>
+                    </li>
+                    <li class="common-info-list scale-list">
+                      <h4 class="ttl">액면가</h4>
+                      <div class="dec">{{'-'}} 원</div>
+                    </li>
+                    <li class="common-info-list scale-list">
+                      <h4 class="ttl">자본금</h4>
+                      <div class="dec">{{'-'}} 원</div>
+                    </li>
+                    <li class="common-info-list scale-list">
+                      <h4 class="ttl">주주수</h4>
+                      <div class="dec">{{'-'}} 명</div>
+                    </li>
+                    <li class="common-info-list scale-list">
+                      <h4 class="ttl">대표보통주식주 비율</h4>
+                      <div class="dec">{{'-'}} %</div>
+                    </li>
+                    <li class="common-info-list scale-list">
+                      <h4 class="ttl">대표주주구분</h4>
+                      <div class="dec">{{'-'}}</div>
+                    </li>
+                    <li class="common-info-list scale-list">
+                      <h4 class="ttl">대표주주명</h4>
+                      <div class="dec">{{'-'}}</div>
+                    </li>
+                    <li class="common-info-list scale-list">
+                      <h4 class="ttl">대표지분율</h4>
+                      <div class="dec">{{'-'}} %</div>
+                    </li>
+                    <li class="common-info-list scale-list">
+                      <h4 class="ttl">종업원 수</h4>
+                      <div class="dec">
+                        <span class="comment">(기준일 : 2000.00.00)</span>
+                        {{'-'}} 명
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div class="etpr-panel-wrap flexable-inner">
+              <div class="panel-ttl">
+                <span>주주정보</span>
+                컴포넌트에서 가져오기
+                    <!-- TODO: 업데이트 데이터. 팩트체커 green, 자사 yellow, 오래된 항목은 badge만 -->
+                <p class="badge yellow">{{'팩트체커 업데이트  2020.00.00'}}</p>
+              </div>
+              <div class="white-panel">
+                <div class="common-info-container">
+                  <p class="common-info-graph">주주정보 들어옵니다.</p>
+                </div>
+              </div>
+              <div class="bot-txt">결산 기준일 : 2021.00.00</div>
+            </div>
+          </div>
+          <div class="flexable-container">
+            <div class="etpr-panel-wrap flexable-inner">
+              <div class="panel-ttl">
+                <span>재무상태</span>
+                컴포넌트에서 가져오기
+                    <!-- TODO: 업데이트 데이터. 팩트체커 green, 자사 yellow, 오래된 항목은 badge만 -->
+                <p class="badge yellow">{{'팩트체커 업데이트  2020.00.00'}}</p>
+              </div>
+              <div class="white-panel">
+                <div class="common-info-container">
+                  <p class="common-info-graph">재무상태 들어옵니다.</p>
+                </div>
+              </div>
+              <div class="bot-txt">결산 기준일 : 2021.00.00</div>
+            </div>
+            <div class="etpr-panel-wrap flexable-inner">
+              <div class="panel-ttl">
+                <span>손익계산서</span>
+                컴포넌트에서 가져오기
+                    <!-- TODO: 업데이트 데이터. 팩트체커 green, 자사 yellow, 오래된 항목은 badge만 -->
+                <p class="badge yellow">{{'팩트체커 업데이트  2020.00.00'}}</p>
+              </div>
+              <div class="white-panel">
+                <div class="common-info-container">
+                  <p class="common-info-graph">손익계산서 들어옵니다.</p>
+                </div>
+              </div>
+              <div class="bot-txt">결산 기준일 : 2021.00.00</div>
+            </div>
+          </div>
+          <div class="flexable-container split3">
+            <div class="etpr-panel-wrap flexable-inner">
+              <div class="panel-ttl">
+                <span>매출 추이</span>
+                컴포넌트에서 가져오기
+                    <!-- TODO: 업데이트 데이터. 팩트체커 green, 자사 yellow, 오래된 항목은 badge만 -->
+                <p class="badge yellow">{{'팩트체커 업데이트  2020.00.00'}}</p>
+              </div>
+              <div class="white-panel">
+                <div class="common-info-container">
+                  <p class="common-info-graph">매출 추이 들어옵니다.</p>
+                </div>
+              </div>
+              <div class="bot-txt">결산 기준일 : 2021.00.00</div>
+            </div>
+            <div class="etpr-panel-wrap flexable-inner">
+              <div class="panel-ttl">
+                <span>영업이익 추이</span>
+                컴포넌트에서 가져오기
+                    <!-- TODO: 업데이트 데이터. 팩트체커 green, 자사 yellow, 오래된 항목은 badge만 -->
+                <p class="badge yellow">{{'팩트체커 업데이트  2020.00.00'}}</p>
+              </div>
+              <div class="white-panel">
+                <div class="common-info-container">
+                  <p class="common-info-graph">영업이익 추이 들어옵니다.</p>
+                </div>
+              </div>
+              <div class="bot-txt">결산 기준일 : 2021.00.00</div>
+            </div>
+            <div class="etpr-panel-wrap flexable-inner">
+              <div class="panel-ttl">
+                <span>당기순이익 추이</span>
+                컴포넌트에서 가져오기
+                    <!-- TODO: 업데이트 데이터. 팩트체커 green, 자사 yellow, 오래된 항목은 badge만 -->
+                <p class="badge yellow">{{'팩트체커 업데이트  2020.00.00'}}</p>
+              </div>
+              <div class="white-panel">
+                <div class="common-info-container">
+                  <p class="common-info-graph">당기순이익 추이 들어옵니다.</p>
+                </div>
+              </div>
+              <div class="bot-txt">결산 기준일 : 2021.00.00</div>
+            </div>
+          </div>
+        </div>
+        <!-- END 상세정보 -->
+      </div>
+      <!-- END 정보 영역 -->
       <layout-footer />
     </div>
   </div>
@@ -175,6 +405,42 @@
 <script>
 export default {
   name: 'EtprProfileBoard',
+  date() {
+    return {
+      mbMdOptions1: {
+        visible: false,
+      },
+      mbMdOptions2: {
+        visible: false,
+      },
+    }
+  },
+  methods: {
+    mbMdOpen(opt) {
+      if(window.innerWidth <= 600) {
+        switch (opt) {
+        case 'star':
+          this.mbMdOptions1.visible = true;
+          break;
+        case 'question':
+          this.mbMdOptions2.visible = true;
+          break;
+        }
+      }
+    },
+    mbMdClose(opt) {
+      if(window.innerWidth <= 600) {
+        switch (opt) {
+        case 'star':
+          this.mbMdOptions1.visible = false;
+          break;
+        case 'question':
+          this.mbMdOptions2.visible = false;
+          break;
+        }
+      }
+    },
+  }
 };
 </script>
 
