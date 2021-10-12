24c1cebc5303700a09d10ba38eb7cd5db2f59ae8
pocket.cgs <pocket.cgs@gmail.com>
2021-10-12 14:23:59 +0900

    cgs - white-container 관련

--- src/views/user-startup/wating/SWatingMain.vue
+++ src/views/user-startup/wating/SWatingMain.vue
@@ -1,7 +1,43 @@
 <template>
   <!-- [D] 마이페이지 - 신청대기중, 마이페이지 - 신청거절/그룹강퇴 -->
-  <div>
-    마이페이지 - 신청대기중, 마이페이지 - 신청거절/그룹강퇴
+  <div class="mypage-container waiting-page">
+    <div class="narrow-panel">
+      <div class="waiting-cont-wrap">
+        <p class="cont-ment">
+          가입신청 대기중입니다.
+        </p>
+        <div class="process-card">
+          <p class="process-ment">
+            신청 대기중인 기업
+          </p>
+          <div class="avatar-with-name col-avatar-with-name">
+            <avatar />
+            <div class="name-grp">
+              <span class="name">
+                주식회사 팩트컴퍼니
+              </span>
+
+              <ul class="name-subtxt-grp">
+                <li class="name-subtxt-list">
+                  기업규모
+                </li>
+                <li class="name-subtxt-list">
+                  산업분류
+                </li>
+                <li class="name-subtxt-list">
+                  산업분야
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <p class="cont-ment-sub">
+          가입신청 대기중입니다.<br>
+          그룹관리자 권한으로 신청 승인이 완료 후<br>
+          팩트시트 서비스를 이용하실 수 있습니다.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
