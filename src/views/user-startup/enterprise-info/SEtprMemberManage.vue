d9e8b287b3579849b6fb60c2ca17a7ee8d895bf0
jeonghyejin <pocket.jhj@gmail.com>
2021-10-12 15:33:53 +0900

    jhj 구성원관리 모달 추가 완료

--- src/views/user-startup/enterprise-info/SEtprMemberManage.vue
+++ src/views/user-startup/enterprise-info/SEtprMemberManage.vue
@@ -49,10 +49,14 @@
           <div class="btn-grp">
             <btn-solid-grayblue
               class="btn-sm"
+              @click="openModal('addMember')"
             >
               구성원 추가
             </btn-solid-grayblue>
-            <btn-bd-light class="btn-sm">
+            <btn-bd-light
+              class="btn-sm"
+              @click="openModal('openSetting')"
+            >
               항목 공개 설정
             </btn-bd-light>
           </div>
@@ -129,6 +133,219 @@
       </div>
     </div>
     <!-- // 마이페이지 컨텐츠 영역 -->
+
+    <modal-dialog>
+      <modal-light-box @click="closeModal" />
+
+      <!-- [modal] 항목 공개 설정 -->
+      <modal
+        :class="{'is-show': $store.state.modal.visible && $store.state.modal.type === 'openSetting'}"
+        ttl="항목 공개 설정"
+      >
+        <div class="modal-cont">
+          <ul class="toggle-list-grp">
+            <li class="toggle-list">
+              <span class="name">이메일</span>
+
+              <!-- 토글 스위치 -->
+              <input
+                id="openEmail"
+                type="checkbox"
+                class="is-none toggle-switch"
+              >
+              <label
+                for="openEmail"
+                class="toggle-onoff"
+              />
+              <!-- // 토글 스위치 -->
+            </li>
+
+            <li class="toggle-list">
+              <span class="name">이름</span>
+
+              <!-- 토글 스위치 -->
+              <input
+                id="openName"
+                type="checkbox"
+                class="is-none toggle-switch"
+              >
+              <label
+                for="openName"
+                class="toggle-onoff"
+              />
+              <!-- // 토글 스위치 -->
+            </li>
+
+            <li class="toggle-list">
+              <span class="name">키포인트</span>
+
+              <!-- 토글 스위치 -->
+              <input
+                id="openKeypoint"
+                type="checkbox"
+                class="is-none toggle-switch"
+              >
+              <label
+                for="openKeypoint"
+                class="toggle-onoff"
+              />
+              <!-- // 토글 스위치 -->
+            </li>
+
+            <li class="toggle-list">
+              <span class="name">이력사항</span>
+
+              <!-- 토글 스위치 -->
+              <input
+                id="openHistory"
+                type="checkbox"
+                class="is-none toggle-switch"
+              >
+              <label
+                for="openHistory"
+                class="toggle-onoff"
+              />
+              <!-- // 토글 스위치 -->
+            </li>
+          </ul>
+        </div>
+
+        <div class="modal-btn-wrap">
+          <btn-bd-light
+            class="btn-fb btn-md btn-cancel"
+            @click="closeModal"
+          >
+            닫기
+          </btn-bd-light>
+          <btn-solid-dark class="btn-md">
+            저장
+          </btn-solid-dark>
+        </div>
+      </modal>
+
+      <!-- [modal] 구성원 추가 -->
+      <modal
+        :class="{'is-show': $store.state.modal.visible && $store.state.modal.type === 'addMember'}"        
+        wide
+        ttl="구성원 추가"
+      >
+        <div class="modal-cont modal-scrl-cont">
+          <form>
+            <fieldset class="ffieldset">
+              <legend class="ffieldset-ttl">
+                프로필 이미지
+              </legend>
+
+              <div class="file-inp-grp">
+                <!-- [D] 해당 DOM 클릭시 사진 업로드 & 수정 가능 -->
+                <div class="input-img-frame input-circle-img-frame">
+                  <!-- [D] 이미지 업로드 시 아래 img 태그 보임 -->
+                  <!-- <img
+                    src="@/assets/images/dft/ex.jpg"
+                    alt="input img"
+                    class="input-img"
+                  > -->
+
+                  <span
+                    class="btn btn-upload"
+                  >
+                    <i class="icon icon-cam" />
+                    <span class="is-voice-only">기업 로고 이미지 업로드</span>
+                  </span>
+                </div>
+                <p class="ffieldset-guide">
+                  해상도 100*100px 이상의 JPG, PNG 파일을 첨부해주세요. <br>
+                  이미지 미첨부시, 기본이미지로 설정됩니다.
+                </p>
+              </div>
+            </fieldset>
+
+            <fieldset class="ffieldset">
+              <legend class="ffieldset-ttl">
+                기본 정보
+              </legend>
+
+              <!-- 이름 -->
+              <div class="lbel-inp-grp">
+                <label class="lbel">이름</label>
+                <div class="inp-grp">
+                  <input
+                    type="text"
+                    class="inp"
+                    placeholder="이름 입력"
+                  >
+                </div>
+              </div>
+
+              <!-- 이메일 -->
+              <div class="lbel-inp-grp">
+                <label class="lbel">이메일</label>
+                <div class="inp-grp">
+                  <input
+                    type="email"
+                    class="inp"
+                    placeholder="이메일 입력"
+                  >
+                </div>
+              </div>
+
+              <!-- 키포인트 -->
+              <div class="lbel-inp-grp">
+                <label class="lbel">키포인트</label>
+                <div class="inp-grp">
+                  <input
+                    type="text"
+                    class="inp"
+                    placeholder="ex) 10개 기업 창업"
+                  >
+                </div>
+              </div>
+
+              <!-- 경력사항 -->
+              <div class="lbel-inp-grp">
+                <label class="lbel">경력사항</label>
+
+                <div class="inp-grp">
+                  <input
+                    type="text"
+                    class="inp"
+                  >
+                </div>
+
+                <div class="add-inp-grp">
+                  <div class="inp-grp inp-btn-grp">
+                    <input
+                      type="text"
+                      class="inp"
+                    >
+                    <btn-square-bd class="del-btn">
+                      <span class="is-voice-only">항목 삭제</span>
+                    </btn-square-bd>
+                  </div>
+                  <!-- 요기에 추가가 되어야합니다 -->
+                </div>
+
+                <btn-bd-dark class="btn-md btn-full add-inp-btn">
+                  경력사항 추가
+                </btn-bd-dark>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+
+        <div class="modal-btn-wrap">
+          <btn-bd-light
+            class="btn-fb btn-md btn-cancel"
+            @click="closeModal"
+          >
+            닫기
+          </btn-bd-light>
+          <btn-solid-dark class="btn-md">
+            추가완료
+          </btn-solid-dark>
+        </div>
+      </modal>
+    </modal-dialog>
   </div>
 </template>
 
