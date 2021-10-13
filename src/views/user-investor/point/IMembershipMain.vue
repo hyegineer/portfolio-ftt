d15361429ba4ff9e6a316492818a5cf751aa2ecd
jeonghyejin <pocket.jhj@gmail.com>
2021-10-13 17:29:03 +0900

    jhj 멤버십 구매 페이지 완료

--- src/views/user-investor/point/IMembershipMain.vue
+++ src/views/user-investor/point/IMembershipMain.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- [D] 마이페이지 - 포인트-멤버십 구매 -->
-  <div class="mypage-container">
+  <div class="mypage-container mypage-membership-main-page">
     <!-- 마이페이지 페이지 제목 -->
     <div class="mypage-hgrp">
       <breadcrumb>
@@ -47,8 +47,252 @@
           </p>
         </section>
       </div>
+      
+      <div class="mypage-point-card">
+        <div class="mypage-point-hgrp">
+          <p class="ttl">
+            <b class="bold">투자 컴퍼니</b>의 <br>멤버십 정보
+          </p>
+
+          <!-- [D] 멤버십 없을 때만 보이고, 있을 땐 안보임 -->
+          <div class="nothing-membership">
+            멤버십이 없습니다.
+          </div>
+
+          <div class="ttl-sub">
+            <label class="lbel">현재 보유 포인트</label>
+
+            <div class="amount-item">
+              <strong class="amount">100,000</strong>
+              <div class="badge-p" />
+            </div>
+          </div>
+          <btn-bd-dark
+            class="charge-btn btn-sm"
+            @click="$router.push('point-charge')"
+          >
+            충전하기
+          </btn-bd-dark>
+        </div>
+
+        <div class="mypage-point-history-wrap">
+          <!-- [D] 멤버십 없을 때만 보이고, 있을 땐 안보임 -->
+          <div class="ttl">
+            우리 기업에 맞는 멤버십을 구매하세요.
+          </div>
+          
+          <div class="membership-list-grp">
+            <!-- [D] 구독중인 멤버십일 경우 is-active 클래스 추가 -->
+            <article
+              class="membership-list is-active"
+            >
+              <div>
+                <div class="mbs-hgrp">
+                  <span class="sub-name">팩트시트</span>
+                  <h6 class="name">
+                    6개월 멤버십
+                  </h6>
+                </div>
+
+                <div class="mbs-benefit-list-grp">
+                  <p class="mbs-benefit-list">
+                    6개월간 멤버십 혜택 이용
+                  </p>
+                  <p class="mbs-benefit-list">
+                    3개월이 지나 블라인드 된 <br>팩트시트를 확인할 수 있습니다.
+                  </p>
+                  <p class="mbs-benefit-list">
+                    스타트업의 업데이트 이력을 확인할 수 있습니다.
+                  </p>
+                </div>
+
+                <div class="mbs-point-grp">
+                  <span class="lbel">사용 포인트</span>
+                  <div class="amount-item">
+                    <strong class="amount">0,000</strong>
+                    <div class="badge-p" />
+                  </div>
+                </div>
+              </div>
+
+              <btn-solid-dark
+                class="buy-btn btn-rounded btn-xsm btn-full" 
+                @click="openModal('buyMembership')"
+              >
+                구매하기
+              </btn-solid-dark>
+
+              <!-- [D] 구독중일 때 버튼 -->
+              <!-- <btn-solid-dark
+                disabled
+                class="buy-btn btn-rounded btn-xsm btn-full"
+              >
+                구독중
+              </btn-solid-dark> -->
+            </article>
+            
+            <article
+              class="membership-list"
+            >
+              <div>
+                <div class="mbs-hgrp">
+                  <span class="sub-name">팩트시트</span>
+                  <h6 class="name">
+                    12개월 멤버십
+                  </h6>
+                </div>
+
+                <div class="mbs-benefit-list-grp">
+                  <p class="mbs-benefit-list">
+                    12개월간 멤버십 혜택 이용
+                  </p>
+                  <p class="mbs-benefit-list">
+                    3개월이 지나 블라인드 된 <br>팩트시트를 확인할 수 있습니다.
+                  </p>
+                  <p class="mbs-benefit-list">
+                    스타트업의 업데이트 이력을 확인할 수 있습니다.
+                  </p>
+                </div>
+
+                <div class="mbs-point-grp">
+                  <span class="lbel">사용 포인트</span>
+                  <div class="amount-item">
+                    <strong class="amount">0,000</strong>
+                    <div class="badge-p" />
+                  </div>
+                </div>
+              </div>
+
+              <btn-solid-dark
+                class="buy-btn btn-rounded btn-xsm btn-full"
+                @click="openModal('buyMembership')"
+              >
+                구매하기
+              </btn-solid-dark>
+            </article>
+            
+            <article
+              class="membership-list"
+            >
+              <div>
+                <div class="mbs-hgrp">
+                  <span class="sub-name">팩트시트</span>
+                  <h6 class="name">
+                    24개월 멤버십
+                  </h6>
+                </div>
+
+                <div class="mbs-benefit-list-grp">
+                  <p class="mbs-benefit-list">
+                    24개월간 멤버십 혜택 이용
+                  </p>
+                  <p class="mbs-benefit-list">
+                    3개월이 지나 블라인드 된 <br>팩트시트를 확인할 수 있습니다.
+                  </p>
+                  <p class="mbs-benefit-list">
+                    스타트업의 업데이트 이력을 확인할 수 있습니다.
+                  </p>
+                </div>
+
+                <div class="mbs-point-grp">
+                  <span class="lbel">사용 포인트</span>
+                  <div class="amount-item">
+                    <strong class="amount">0,000</strong>
+                    <div class="badge-p" />
+                  </div>
+                </div>
+              </div>
+
+              <btn-solid-dark
+                class="buy-btn btn-rounded btn-xsm btn-full" 
+                @click="openModal('buyMembership')"
+              >
+                구매하기
+              </btn-solid-dark>
+            </article>
+          </div>
+        </div>
+      </div>
     </div>
     <!-- // 마이페이지 컨텐츠 영역 -->
+
+    <modal-dialog>
+      <modal-light-box @click="closeModal" />
+
+      <!-- [modal] 멤버십 구매하기 -->
+      <modal
+        :class="{'is-show': $store.state.modal.visible && $store.state.modal.type === 'buyMembership'}"
+        wide
+        ttl="'12개월 멤버십'을 <br>구매하시겠습니까?"
+      >
+        <div class="modal-cont md-buy-membership">
+          <div class="lbel-amount-point-grp">
+            <div class="lbel-amount-point">
+              <span class="lbel">현재 보유 포인트</span>
+
+              <div class="amount-point">
+                <strong class="amount">150</strong>
+                <div class="badge-p">
+                  <span class="is-voice-only">포인트</span>
+                </div>
+              </div>
+            </div>
+            <div class="lbel-amount-point">
+              <span class="lbel">구매 후 포인트</span>
+
+              <div class="amount-point">
+                <strong class="amount">150</strong>
+                <div class="badge-p">
+                  <span class="is-voice-only">포인트</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="lbel-inp-grp">
+            <label class="lbel">약관</label>
+
+            <textarea
+              class="inp"
+              cols="30"
+              rows="10"
+              readonly              
+            >
+            약관 txt
+            </textarea>
+          </div>
+
+          <div class="check-lbel-ment">
+            <input
+              id="agreeBuyMbs"
+              type="checkbox"
+              class="is-none check-switch"
+            >
+            <label
+              for="agreeBuyMbs"
+              class="check-onoff"
+            />
+
+            <label
+              for="agreeBuyMbs"
+              class="ment dark-ment"
+            >위 내용을 확인하였으며, 구매를 진행합니다.</label>
+          </div>
+        </div>
+
+        <div class="modal-btn-wrap">
+          <btn-bd-light
+            class="btn-fb btn-md btn-cancel"
+            @click="closeModal"
+          >
+            닫기
+          </btn-bd-light>
+          <btn-solid-dark class="btn-md">
+            구매하기
+          </btn-solid-dark>
+        </div>
+      </modal>
+    </modal-dialog>
   </div>
 </template>
 
