f0eed171a39d2cfd696c3f1a8e1c35751806aa73
pocket.cgs <pocket.cgs@gmail.com>
2021-10-20 17:03:35 +0900

    cgs - 1200px 체크

--- src/views/user-investor/wating/IWatingComplete.vue
+++ src/views/user-investor/wating/IWatingComplete.vue
@@ -5,20 +5,23 @@
     class="mypage-container"
   >
     <div class="narrow-panel auth-panel">
-      <div class="waiting-complete-cont-wrap">
-        <div class="auth-hgrp">
+      <!-- TODO: 기존 스타트업 신청 -->
+      <div
+        v-if="testApplyType === 'old'"
+        class="waiting-complete-cont-wrap"
+      >
+        <div
+          class="auth-hgrp"
+        >
           <img
-            src="@/assets/images/badge/badge-investror.svg"
+            src="@/assets/images/badge/badge-startup.svg"
             alt=""
             class="badge-flag"
           >
         
           <h3 class="ttl">
-            <!-- 투자자 신규등록일 때 -->
-            <!-- <span class="ttl-txt">투자자 신규등록</span> -->
-            
-            <!-- 기존 투자자 신청일 때 -->
-            <span class="ttl-txt">기존 투자자 신청</span>
+            <!-- 기존 스타트업 신청일 때 -->
+            <span class="ttl-txt">기존 스타트업 신청</span>
             <span class="badge-auth">직원</span>
           <!-- <span class="badge-auth">{{ $constants.userTypeForm[$route.query.userTypeForm] }}</span> -->
           </h3>
@@ -28,7 +31,7 @@
             신청이 완료되었습니다.
           </p>
           <p class="ment-sub">
-            투자자 가입신청이 완료되었습니다. <br>
+            스타트업 가입신청이 완료되었습니다.<br>
             신청 허가 완료시, 팩트시트 서비스를 이용하실 수 있습니다.
           </p>
         </div>
@@ -41,6 +44,47 @@
           </btn-solid>
         </div>
       </div>
+      <!-- TODO: END 기존 스타트업 신청 -->
+
+      <!-- TODO: 스타트업 신규등록 -->
+      <div
+        v-if="testApplyType === 'new'"
+        class="waiting-complete-cont-wrap"
+      >
+        <div
+          class="auth-hgrp"
+        >
+          <img
+            src="@/assets/images/badge/badge-startup.svg"
+            alt=""
+            class="badge-flag"
+          >
+        
+          <h3 class="ttl">
+            <span class="ttl-txt">스타트업 신규등록</span>
+            <span class="badge-auth">직원</span>
+          <!-- <span class="badge-auth">{{ $constants.userTypeForm[$route.query.userTypeForm] }}</span> -->
+          </h3>
+        </div>
+        <div class="ment-wrap">
+          <p class="ment">
+            등록이 완료되었습니다.
+          </p>
+          <p class="ment-sub">
+            스타트업 신규등록이 완료되었습니다.<br>
+            '확인'버튼을 누르면 팩트시트 서비스를 이용하실 수 있습니다.
+          </p>
+        </div>
+        <div class="btn-wrap">
+          <btn-solid
+            class="btn btn-lg btn-full"
+            @click="$router.push('/')"
+          >
+            확인
+          </btn-solid>
+        </div>
+      </div>
+      <!-- TODO: END 스타트업 신규등록 -->
     </div>
   </div>
 </template>
@@ -48,6 +92,11 @@
 <script>
 export default {
   name: 'IWatingComplete',
+  data() {
+    return {
+      testApplyType: 'old', // new: 스타트업 신규등록, old: 기존 스타트업 신청 
+    };
+  },
 };
 </script>
 
