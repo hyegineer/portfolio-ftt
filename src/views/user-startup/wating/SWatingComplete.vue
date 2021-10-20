7d7a937206f10996739c71212978d027a71caf0b
pocket.cgs <pocket.cgs@gmail.com>
2021-10-20 16:16:24 +0900

    cgs - 해당 페이지 분기처리 수정

--- src/views/user-startup/wating/SWatingComplete.vue
+++ src/views/user-startup/wating/SWatingComplete.vue
@@ -4,9 +4,17 @@
     mypage="waiting-complete-page"
     class="mypage-container"
   >
-    <div class="narrow-panel auth-panel">
-      <div class="waiting-complete-cont-wrap">
-        <div class="auth-hgrp">
+    <div
+      class="narrow-panel auth-panel"
+    >
+      <!-- TODO: 기존 스타트업 신청 -->
+      <div
+        v-if="testApplyType === 'old'"
+        class="waiting-complete-cont-wrap"
+      >
+        <div
+          class="auth-hgrp"
+        >
           <img
             src="@/assets/images/badge/badge-startup.svg"
             alt=""
@@ -14,9 +22,6 @@
           >
         
           <h3 class="ttl">
-            <!-- 스타트업 신규등록일 때 -->
-            <span class="ttl-txt">스타트업 신규등록</span>
-
             <!-- 기존 스타트업 신청일 때 -->
             <span class="ttl-txt">기존 스타트업 신청</span>
             <span class="badge-auth">직원</span>
@@ -41,6 +46,47 @@
           </btn-solid>
         </div>
       </div>
+      <!-- TODO: END 기존 스타트업 신청 -->
+
+      <!-- TODO: 스타트업 신규등록 -->
+      <div
+        v-if="testApplyType === 'new'"
+        class="waiting-complete-cont-wrap"
+      >
+        <div
+          class="auth-hgrp"
+        >
+          <img
+            src="@/assets/images/badge/badge-startup.svg"
+            alt=""
+            class="badge-flag"
+          >
+        
+          <h3 class="ttl">
+            <span class="ttl-txt">스타트업 신규등록</span>
+            <span class="badge-auth">직원</span>
+          <!-- <span class="badge-auth">{{ $constants.userTypeForm[$route.query.userTypeForm] }}</span> -->
+          </h3>
+        </div>
+        <div class="ment-wrap">
+          <p class="ment">
+            등록이 완료되었습니다.
+          </p>
+          <p class="ment-sub">
+            스타트업 신규등록이 완료되었습니다.<br>
+            '확인'버튼을 누르면 팩트시트 서비스를 이용하실 수 있습니다.
+          </p>
+        </div>
+        <div class="btn-wrap">
+          <btn-solid
+            class="btn btn-lg btn-full"
+            @click="$router.push('/')"
+          >
+            확인
+          </btn-solid>
+        </div>
+      </div>
+      <!-- TODO: END 스타트업 신규등록 -->
     </div>
   </div>
 </template>
@@ -48,6 +94,11 @@
 <script>
 export default {
   name: 'SWatingComplete',
+  data() {
+    return {
+      testApplyType: 'old', // new: 스타트업 신규등록, old: 기존 스타트업 신청 
+    };
+  },
 };
 </script>
 
